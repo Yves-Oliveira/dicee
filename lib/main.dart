c678b8b105ab5d00f7a2ff059e05eaa644f68c2b
Yves de Oliveira Ribeiro <72031760+Yves-Oliveira@users.noreply.github.com>
2021-08-29 05:06:51 -0300

    .

--- lib/main.dart
+++ lib/main.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 void main() {
   return runApp(
     MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Colors.red,
         appBar: AppBar(
@@ -15,9 +16,36 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int leftDiceNumber = 3;
   @override
   Widget build(BuildContext context) {
-    return Row();
+    return Center(
+      child: Row(
+        children: [
+          Expanded(
+              child: TextButton(
+                  onPressed: () {
+                    setState(() {
+                      leftDiceNumber = 5;
+                    });
+                  },
+                  child: Image.asset('images/dice$leftDiceNumber.png'))),
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                print('The righ button was pressed');
+              },
+              child: Image.asset('images/dice2.png'),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
